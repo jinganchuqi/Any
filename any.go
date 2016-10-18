2d0e34106b835d0446b821a9c6506e4ec2d9fbe2
jinganchuqi <314121405@qq.com>
2016-10-18 17:00:56 +0800

    新增view

---
+++ any.go
@@ -0,0 +1,68 @@
+package any
+
+import (
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
+)
+
+type App struct {
+	RoutersMap Routers
+	Ctx        HttpContext
+}
+
+func (app *App) Run() {
+	http.HandleFunc("/", app.callHttp)
+	err := http.ListenAndServe(":88", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func (app *App) LoadRoute(routersMap Routers) *App {
+	app.RoutersMap = routersMap
+	return app
+}
+
+func (app *App) callHttp(res http.ResponseWriter, req *http.Request) {
+	app.Ctx = HttpContext{ResponseWriter: res, Request: req}
+	isFound := false
+	switch req.Method {
+	case "GET":
+		isFound = app.switchMethod(app.RoutersMap.GetMap)
+	case "POST":
+		isFound = app.switchMethod(app.RoutersMap.PostMap)
+
+	}
+	if isFound == false {
+		http.NotFound(res, req)
+	}
+}
+
+func (app *App) switchMethod(routerMap map[string]interface{}) bool {
+	if len(routerMap) < 1 {
+		return false
+	}
+	method := strings.Trim(app.Ctx.Request.URL.Path, "/")
+	for k, v := range routerMap {
+		path := strings.Split(k, "@")
+		if method == strings.Trim(path[0], "/") {
+			vt := reflect.Indirect(reflect.ValueOf(v)).Type()
+			vc := reflect.New(vt)
+			init := vc.MethodByName("Init")
+			in := make([]reflect.Value, 1)
+			in[0] = reflect.ValueOf(app.Ctx)
+			init.Call(in)
+			in = make([]reflect.Value, 0)
+			opt := vc.MethodByName(path[1])
+
+			if opt.IsValid() == true {
+				opt.Call(in)
+				return true
+			}
+			break
+		}
+	}
+	return false
+}
